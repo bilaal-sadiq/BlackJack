efc9694cdd39489236f4c7ee5d8b2a2164105afb
mrmbs1 <mbilalsadiq.19@gmail.com>
2025-04-29 13:44:18 +0100

    else if statement

---
+++ index.js
@@ -0,0 +1,19 @@
+// let firstCard = 6
+// let secondCard = 6
+
+// let sumOfCard = firstCard + secondCard
+
+// console.log(sumOfCard)
+
+
+let age = 100
+let myAge = 26
+
+if (myAge < age) {
+    console.log("Not yet")
+}
+else if (myAge > age) {
+    console.log("Here is your burtday card from the king")
+}
+else (age > 101)
+    
